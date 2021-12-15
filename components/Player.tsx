046846279f22808a0272f8792b1d7cb9569aa3ad
bgutsol <bohdan.hutsol@contentful.com>
2021-12-15 19:13:50 +0100

    feat: player logic

--- components/Player.tsx
+++ components/Player.tsx
@@ -22,46 +22,75 @@ import {
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
 
-const Player = () => {
+const Player = ({ songs, activeSong }) => {
+  const [playing, setPlaying] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [seek, setSeek] = useState(0.0);
+  const [repeat, setRepeat] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+  const [duration, setDuration] = useState(0.0);
+
+  const setPlayState = (value) => {
+    setPlaying(value);
+  };
+
+  const onShuffle = () => {
+    setShuffle((state) => !state);
+  };
+
+  const onRepeat = () => {
+    setRepeat((state) => !state);
+  };
+
   return (
     <Box>
-      <Box>{/* <ReactHowler /> */}</Box>
-      <Center color="green.600">
+      <Box>
+        <ReactHowler playing={playing} src={activeSong?.url} />
+      </Box>
+      <Center color="gray.600">
         <ButtonGroup>
           <IconButton
             outline="none"
             variant="link"
             aria-label="shuffle"
             fontSize="24px"
+            color={shuffle ? "white" : "gray.600"}
+            onClick={onShuffle}
             icon={<MdShuffle />}
           />
           <IconButton
             outline="none"
             variant="link"
-            aria-label="skip previous"
+            aria-label="skip"
             fontSize="24px"
             icon={<MdSkipPrevious />}
           />
+          {playing ? (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="pause"
+              fontSize="40px"
+              color="white"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={() => setPlayState(false)}
+            />
+          ) : (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="play"
+              fontSize="40px"
+              color="white"
+              icon={<MdOutlinePlayCircleFilled />}
+              onClick={() => setPlayState(true)}
+            />
+          )}
+
           <IconButton
             outline="none"
             variant="link"
-            aria-label="play"
-            fontSize="40px"
-            color="white"
-            icon={<MdOutlinePlayCircleFilled />}
-          />
-          <IconButton
-            outline="none"
-            variant="link"
-            aria-label="pause"
-            fontSize="40px"
-            color="white"
-            icon={<MdOutlinePauseCircleFilled />}
-          />
-          <IconButton
-            outline="none"
-            variant="link"
-            aria-label="skip next"
+            aria-label="next"
             fontSize="24px"
             icon={<MdSkipNext />}
           />
@@ -70,10 +99,13 @@ const Player = () => {
             variant="link"
             aria-label="repeat"
             fontSize="24px"
+            color={repeat ? "white" : "gray.600"}
+            onClick={onRepeat}
             icon={<MdOutlineRepeat />}
           />
         </ButtonGroup>
       </Center>
+
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
@@ -81,20 +113,20 @@ const Player = () => {
           </Box>
           <Box width="80%">
             <RangeSlider
-              aria-label={["min", "max"] as any}
+              aria-label={["min", "max"]}
               step={0.1}
               min={0}
               max={321}
               id="player-range"
             >
               <RangeSliderTrack bg="gray.800">
-                <RangeSliderFilledTrack bg="green.600" />
+                <RangeSliderFilledTrack bg="gray.600" />
               </RangeSliderTrack>
               <RangeSliderThumb index={0} />
             </RangeSlider>
           </Box>
-          <Box>
-            <Text fontSize="xs">3:21</Text>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">321</Text>
           </Box>
         </Flex>
       </Box>
